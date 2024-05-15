c8641a1beb16826db2ce687baec4024926c814de
sohamtalukdar <103217802+sohamtalukdar@users.noreply.github.com>
2024-05-16 01:16:20 +0530

    remvoing timeline

--- my-app/src/OutlinedTimeline.js
+++ my-app/src/OutlinedTimeline.js
@@ -1,36 +1,36 @@
-import * as React from 'react';
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import Tooltip from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
+// import * as React from 'react';
+// import Timeline from '@mui/lab/Timeline';
+// import TimelineItem from '@mui/lab/TimelineItem';
+// import TimelineSeparator from '@mui/lab/TimelineSeparator';
+// import TimelineConnector from '@mui/lab/TimelineConnector';
+// import TimelineContent from '@mui/lab/TimelineContent';
+// import TimelineDot from '@mui/lab/TimelineDot';
+// import Tooltip from '@mui/material/Tooltip';
+// import Typography from '@mui/material/Typography';
 
-export default function OutlinedTimeline() {
-  return (
-    <Timeline position="alternate">
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <Tooltip 
-          title={
-            <React.Fragment>
-              <Typography color="inherit">2019 Highlights</Typography>
-              <p>Here are the major events of 2019...</p>
-            </React.Fragment>
-          }
-          enterDelay={500} 
-          leaveDelay={200} 
-          placement="top"
-        >
-          <TimelineContent>2019</TimelineContent>
-        </Tooltip>
-      </TimelineItem>
-      {/* Repeat for other TimelineItems with respective Tooltips */}
-    </Timeline>
-  );
-}
+// export default function OutlinedTimeline() {
+//   return (
+//     <Timeline position="alternate">
+//       <TimelineItem>
+//         <TimelineSeparator>
+//           <TimelineDot variant="outlined" />
+//           <TimelineConnector />
+//         </TimelineSeparator>
+//         <Tooltip 
+//           title={
+//             <React.Fragment>
+//               <Typography color="inherit">2019 Highlights</Typography>
+//               <p>Here are the major events of 2019...</p>
+//             </React.Fragment>
+//           }
+//           enterDelay={500} 
+//           leaveDelay={200} 
+//           placement="top"
+//         >
+//           <TimelineContent>2019</TimelineContent>
+//         </Tooltip>
+//       </TimelineItem>
+//       {/* Repeat for other TimelineItems with respective Tooltips */}
+//     </Timeline>
+//   );
+// }
