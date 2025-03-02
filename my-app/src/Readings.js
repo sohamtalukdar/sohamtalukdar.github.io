2455ef6b59704bb558fefc54649567ea989bd7d8
sohamtalukdar <sohamt09@gmail.com>
2025-03-02 21:21:18 +0530

    added

--- my-app/src/Readings.js
+++ my-app/src/Readings.js
@@ -1,20 +1,35 @@
-// Readings.js
 import React from 'react';
 
 function Readings() {
+  const readings = [
+    {
+      title: "How to Train Really Large Models on Many GPUs?",
+      link: "https://lilianweng.github.io/posts/2021-09-25-train-large/",
+    },
+    {
+      title: "What Every Programmer Should Know About Memory",
+      link: "https://people.freebsd.org/~lstewart/articles/cpumemory.pdf",
+    },
+  ];
+
   return (
     <main className="page-content">
-        <section className="wrapper">
-        <h1>Readings</h1>
-        <h4>A Curated list of readings that I found helpful:</h4>
+      <section className="wrapper">
+        <header>
+          <h1>Readings</h1>
+          <h4>A curated list of readings that I found helpful:</h4>
+        </header>
+        
         <ul>
-
-        <li><a href="https://lilianweng.github.io/posts/2021-09-25-train-large/">How to Train Really Large Models on Many GPUs?</a></li>
-        <li><a href="https://people.freebsd.org/~lstewart/articles/cpumemory.pdf">What Every Programmer Should Know About Memory</a></li>
-
-            </ul>
-
-        </section>
+          {readings.map((reading, index) => (
+            <li key={index}>
+              <a href={reading.link} target="_blank" rel="noopener noreferrer">
+                {reading.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
